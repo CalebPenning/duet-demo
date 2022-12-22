ec3e2139d8d59ada4f8357d940388409f5dfdb09
Caleb Penning <cpenning@>
2022-12-21 17:37:54 -0800

    feature/add-help-page: create help page
    Signed-off-by: Paleb Cenning <pcenning@>

---
+++ src/routes/help/+page.svelte
@@ -0,0 +1,1 @@
+<h2>hello i need help</h2>
--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,7 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from '@playwright/test'
+import type { Page } from '@playwright/test'
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
-});
+test('index page has expected h1', async ({ page }: { page: Page }) => {
+	await page.goto('/')
+	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit')
+})
