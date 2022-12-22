5a946f75607f7c5b9c89288e8918b88d973853a2
Caleb Penning <calebpenning@mail.com>
2022-12-21 18:23:49 -0800

    feat/add-answer-page: add test
    Signed-off-by: Paleb Cenning <palebcenning@mail.com>

--- tests/test.ts
+++ tests/test.ts
@@ -10,3 +10,8 @@ test('/help page has an h2', async ({ page }: { page: Page }) => {
 	await page.goto('/help')
 	expect(await page.textContent('h2')).toBe('Hello, I need help!')
 })
+
+test('/answer talks about love and being in love', async ({ page }: { page: Page }) => {
+	await page.goto('/answer')
+	expect(await page.textContent('h1')).toContain('love')
+})
